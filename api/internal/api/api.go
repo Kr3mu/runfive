dbb32f7f773b6bee3c7ec29a31c4377a015303ae
kr3mu <kr3mu.mail@gmail.com>
2026-04-07 21:39:27 +0200

    feat: create huma and fiber entrypoint

--- api/internal/api/api.go
+++ api/internal/api/api.go
@@ -1,1 +1,20 @@
 package api
+
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humafiber"
+	"github.com/gofiber/fiber/v3"
+)
+
+func New(appConfig fiber.Config, humaConfig huma.Config) *fiber.App {
+	app := fiber.New(appConfig)
+	humaApp := humafiber.New(app, humaConfig)
+
+	SetupRoutes(&humaApp)
+
+	return app
+}
+
+func SetupRoutes(app *huma.API) {
+
+}
--- api/main.go
+++ api/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/Kr3mu/runfive/internal/api"
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/gofiber/fiber/v3"
+)
+
+var appConfig = fiber.Config{
+	ServerHeader: "RunFive API",
+	AppName:      "RunFive API",
+}
+
+var listenConfig = fiber.ListenConfig{
+	DisableStartupMessage: true,
+}
+
+var humaConfig = huma.DefaultConfig("RunFive API", "0.01")
+
+var listenPort = flag.String("port", "5000", "Starting ")
+
+func main() {
+	app := api.New(appConfig, humaConfig)
+
+	log.Printf("Serving backend on: %s", *listenPort)
+	log.Fatal(app.Listen(":"+*listenPort, listenConfig))
 }
