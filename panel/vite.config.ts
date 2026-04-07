875a44c2b6c27860e1b1ccf60c8ab31d9f5828f8
kr3mu <kr3mu.mail@gmail.com>
2026-04-07 18:50:47 +0200

    chore: change vite dev server to port 3000

--- panel/vite.config.ts
+++ panel/vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 
 export default defineConfig({
     plugins: [tailwindcss(), svelte(), router()],
+    server: {
+        port: 3000,
+    },
     resolve: {
         alias: {
             $lib: path.resolve("./src/lib"),
